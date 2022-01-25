f11431b07edff6377048cc75cfd327e89e371d0e
Paweł Bęza <pawel.beza99@gmail.com>
2022-01-25 15:08:13 +0100

    Refactor SplashScreen styles

--- src/screens/SplashScreen/SplashScreen.js
+++ src/screens/SplashScreen/SplashScreen.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-
 import LottieView from 'lottie-react-native';
+import styles from './SplashScreenStyle';
 
 class SplashScreen extends React.Component {
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
+      <View style={styles.splashScreen}>
         <LottieView
           source={require('../../assets/animations/splash.json')}
           autoPlay
           loop={false}
           onAnimationFinish={() => this.props.navigation.replace('Detector')}
         />
-        <Text
-          style={{
-            marginTop: 220,
-            fontFamily: 'FreestyleScript',
-            fontSize: 70,
-            fontWeight: '300',
-          }}>
-          Fashion Lens
-        </Text>
+        <Text style={styles.title}>Fashion Lens</Text>
       </View>
     );
   }
---
+++ src/screens/SplashScreen/SplashScreenStyle.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  splashScreen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    marginTop: 220,
+    fontFamily: 'FreestyleScript',
+    fontSize: 70,
+    fontWeight: '300',
+  },
+});
+
+export default styles;
